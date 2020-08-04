c48ae80c3390b657b4171b16a14fb960691f2752
Tyler Huffman <ty.huff24@gmail.com>
2020-08-04 17:35:00 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    array = self.split(" ")
+    #binding.pry
+    array2 = array.find_all do |index|
+      #binding.pry
+      index.include?("!") || index.include?("?") || index.include?(".")
+    end
+    array2.length
   end
 end
